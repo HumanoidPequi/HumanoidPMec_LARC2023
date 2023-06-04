1395fc16a78b2d82044f7a37fbbf2c52d3ed6c20
lucasolives <108996886+lucasolives@users.noreply.github.com>
2023-06-04 10:58:13 -0300

    Update controle_baixo_nivel.ino

--- Hardware/controle_baixo_nivel.ino
+++ Hardware/controle_baixo_nivel.ino
@@ -1,6 +1,6 @@
 #include <Dynamixel2Arduino.h>
 #include <ros.h>
-#include <std_msgs/UInt16MultiArray.h>
+#include <std_msgs/Int16MultiArray.h>
 #include <ArduinoHardware.h>
 #define DXL_SERIAL Serial2
 const int DXL_DIR_PIN = 13; // DYNAMIXEL Shield DIR PIN
@@ -9,22 +9,24 @@ const uint8_t DXL_ID_CNT = 2;
 const uint8_t DXL_ID_LIST[DXL_ID_CNT] = {1, 2};
 const float DXL_PROTOCOL_VERSION = 1.0;
 
+const uint8_t base_servo[DXL_ID_CNT] = {180, 180};
+
 Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
 
 ros::NodeHandle nh;
 
-void servo_cb(const std_msgs::UInt16MultiArray& cmd_msg) {
+void servo_cb(const std_msgs::Int16MultiArray& cmd_msg) {
   if (cmd_msg.data_length != DXL_ID_CNT) {
     return;
   }
 
   for (int i = 0; i < DXL_ID_CNT; i++) {
-    dxl.setGoalPosition(DXL_ID_LIST[i], cmd_msg.data[i], UNIT_DEGREE);
+    dxl.setGoalPosition(DXL_ID_LIST[i], base_servo[i] + cmd_msg.data[i], UNIT_DEGREE);
     delay(0.1);
   }
 }
 
-ros::Subscriber<std_msgs::UInt16MultiArray> sub("servo", servo_cb);
+ros::Subscriber<std_msgs::Int16MultiArray> sub("servo", servo_cb);
 
 void setup() {
   Serial.begin(57600);
@@ -39,6 +41,11 @@ void setup() {
     dxl.torqueOff(DXL_ID_LIST[i]);
     dxl.setOperatingMode(DXL_ID_LIST[i], OP_POSITION);
     dxl.torqueOn(DXL_ID_LIST[i]);
+  }
+  
+  for (int i = 0; i < DXL_ID_CNT; i++) {
+    dxl.setGoalPosition(DXL_ID_LIST[i], base_servo[i], UNIT_DEGREE);
+    delay(0.1);
   }
 }
 
